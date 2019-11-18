46a20f6a5b414c21709cb354ea9baf32eb03e661
Diego <UO251682@uniovi.es>
2019-11-18 14:34:01 +0100

    Controladores de acuerdo a diagrama de paquetes

--- controladores/routes.js
+++ controladores/routes.js
@@ -1,8 +1,8 @@
-const categorias = require('./gest_torneos/model/Categoria');
+const categorias = require('../gest_torneos/model/Categoria');
 const ObjectID = require("mongodb").ObjectID;
-const FutbolFactory = require('./gest_torneos/model/factory/FutbolFactory');
+const FutbolFactory = require('../gest_torneos/model/factory/FutbolFactory');
 const futbolFactory = new FutbolFactory();
-const TorneoFutbol = require('./gest_torneos/model/TorneoFutbol');
+const TorneoFutbol = require('../gest_torneos/model/TorneoFutbol');
 const dotenv = require('dotenv');
 dotenv.config();
 
